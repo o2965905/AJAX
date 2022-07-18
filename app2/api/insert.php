6490f30dd9f93a55c4cb36127e4ea722e56c5a38
o2965905 <o2965905@gmail.com>
2022-07-18 11:33:24 +0800

    完成用AJAX新增學生資料+使用BS美化網頁外觀

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,15 @@
 <?php
 //處理新增資料的請求
+// print_r($_POST);
 
+$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$pdo=new PDO($dsn,'root','');
+
+$sql="insert into students (`classroom`,`seat_num`,`name`,`birthday`) 
+                    values ('{$_POST['classroom']}',
+                            '{$_POST['seatnum']}',
+                            '{$_POST['name']}',
+                            '{$_POST['birthday']}')";
+echo $pdo->exec($sql);
 
 ?>
